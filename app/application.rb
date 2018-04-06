714cc7b8e5671db2b72c57b9009a4efff896a493
Frank McCallion <fkmccallion@gmail.com>
2018-04-06 03:02:11 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@items.select {|item| item.name == item_name}.first
-      item.price
+      resp.write item.price
       #binding.pry
     else
       resp.status = 404
