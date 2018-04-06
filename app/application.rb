2e0125e350d16759b4f6692f5d4f48789a116619
Frank McCallion <fkmccallion@gmail.com>
2018-04-06 02:58:28 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
 
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      price = @@items.collect {|item| item.name == item_name}
+      price = @@items.select {|item| item.name == item_name}
       binding.pry
     else
       resp.status = 404
