1ccbb3ba7b99d2c023a09e8c496215258815eb7c
Frank McCallion <fkmccallion@gmail.com>
2018-04-06 02:41:57 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,18 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      @@items.each do |item|
+        resp.write "#{item}\n"
+      end
+    else
+      resp.write "Path Not Found"
+    end
+
+    resp.finish
+  end
+
+end
