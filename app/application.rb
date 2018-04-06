bbdf657967aaaeeda34371146931de5a0b9750f8
Frank McCallion <fkmccallion@gmail.com>
2018-04-06 02:49:23 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
 
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-
+      binding.pry
     else
       resp.status = 404
       resp.write "Route not found"
